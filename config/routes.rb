833ce085baa66cdccc53c007a2c7c195f0be8f0b
cookingbo <ta99v2@gmail.com>
2022-06-04 12:04:31 +0000

    [Add]devise(Public,Admin)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # 管理者側では新規登録、パスワード変更のルーティングをskipで削除
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 会員側ではパスワード変更のルーティングをskipで削除
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
