0bf5eeaf2d82f651e92399a9353269752e376a80
cookingbo <ta99v2@gmail.com>
2022-06-05 07:16:24 +0000

    [update]管理者パスワード、route指定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destory
   has_many :likes, dependent: :destroy
-
+  
+  # 画像投稿のimageカラムとして扱う
   has_one_attached :image
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_023549) do
+ActiveRecord::Schema.define(version: 2022_06_05_043123) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(version: 2022_06_05_023549) do
     t.integer "user_id", null: false
     t.string "place", null: false
     t.text "explaination", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
