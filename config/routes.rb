a7db2ab6671eeb5747c202745c4e90e20a974efd
cookingbo <ta99v2@gmail.com>
2022-06-05 04:49:45 +0000

    [Add]relationships_controller,route

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,28 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  get '/' => "homes#top"
+  get '/about' => "homes#about"
+
   # 会員側ルーティング
   namespace :public do
+    # postsに対してlikesとpost_commentsをネストする。
     resources :posts, only: [:new, :create, :index, :edit, :update, :destroy] do
-      resource :likes, only: [:create, :destroy]
+      resource :likes, only: [:create, :destroy] # 1つの投稿に対して1回だけしかいいねできないため、resourceとすることでいいねのidを含めない形にした
       resources :post_comments, only: [:create, :destroy]
+    end
+    # usersに対してrelationshipsをネストする。
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy] # 1人のユーザに対して一度しかフォローできないため、resourceとすることでidを含めない形にした。
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#follwers", as: "follwers"
     end
   end
 
   # 管理者側ルーティング
   namespace :admin do
     resources :posts, only: [:index, :show]
+    resources :users, only: [:index, :show, :edit, :update]
   end
 
 
