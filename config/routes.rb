eb29aa28dacf0af24b1ba5112f328acf5341471a
cookingbo <ta99v2@gmail.com>
2022-06-05 02:20:04 +0000

    [Add]post_controller,model,view,migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # 管理者側では新規登録、パスワード変更のルーティングをskipで削除
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -9,7 +10,17 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  
+
+  # 会員側ルーティング
+  namespace :public do
+    resources :posts, only: [:new, :create, :index, :edit, :update, :destroy]
+  end
+
+  # 管理者側ルーティング
+  namespace :admin do
+    resources :posts, only: [:index, :show]
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20220605021627_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id, null: false
+      t.string :place, null: false
+      t.text :explaination, null: false
+
+      t.timestamps
+    end
+  end
+end
