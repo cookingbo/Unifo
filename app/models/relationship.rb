dff892123460b83756ab608185d492bdac3cb186
cookingbo <ta99v2@gmail.com>
2022-06-06 15:04:56 +0000

    [Add]フォロー機能(エラーあり)

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,8 @@
 class Relationship < ApplicationRecord
-  
-  
-  
+
+  # 本来ならbelongs_to :userだがどっちのユーザかわからなくなるため使い分けている
+  # しかしデータはuserテーブルからなのでclass_nameの記述を行う
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :likes,         dependent: :destroy
 
+  # フォローしたユーザ(foreign_key(親子関係)でrelationshipのカラムを取ってくる)
+  has_many :relationships, class_name: "Relationship", foreign_key: follower_id, dependent: :destroy
+  # フォローされたユーザ(foreign_key(親子関係)でrelationshipのカラムを取ってくる)
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: followed_id, dependent: :destroy
+  # フォロー
+  has_many :followings, through: :relationships, source: :followed
+  # フォロワー
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   has_one_attached :profile_image
 
   def get_profile_image
@@ -18,4 +27,16 @@ class User < ApplicationRecord
     self.last_name + " " + self.first_name
   end
 
+  # フォローした時
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+　# フォローを外すとき
+　def unfollow(user_id)
+　  relationships.find_by(followed_id: user_id).destroy
+　end
+　# フォローしているか判定
+　def following?(user)
+　  followings.include?(user)
+　end
 end
