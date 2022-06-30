be2f6b8f7d96eba3bdd0ae94be02f4a18b49784d
cookingbo <ta99v2@gmail.com>
2022-06-30 09:23:27 +0000

    [update]ログイン順に並べる

--- db/migrate/20220609102430_add_country_code_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddCountryCodeToUsers < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -103,6 +103,11 @@ ActiveRecord::Schema.define(version: 2022_06_13_050825) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name", null: false
     t.string "university", null: false
     t.integer "area", null: false
